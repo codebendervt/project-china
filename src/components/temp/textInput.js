9fadd91348926cc9460845ce0bb08bbd9345f881
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2020-07-28 07:24:51 +0200

    removed temp files

--- src/components/temp/textInput.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const TextInput = ({ value, onChangeText }) => (
-    <input type="text" value={value} onChange={e => onChangeText(e.target.value)} />
-  );
-
-  
-  export default TextInput;
